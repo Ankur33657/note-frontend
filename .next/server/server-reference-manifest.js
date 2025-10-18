c5775a178be4956151c92d46db3fe31dbe6d830c
Ankur33657 <ankursingh943040@gmail.com>
2025-10-18 10:55:03 +0530

    feat:remove .next folder

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6BO6TTZFyEpgjDOVWb25vBDOgMOQ2kJ21lS8CQx+c/g=\"\n}"
--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
-  "static/chunks/node_modules_0587691f._.js",
-  "static/chunks/common_186bfb39._.js"
-],
-    source: "dynamic"
-});
