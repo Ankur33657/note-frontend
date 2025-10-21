d78445b0aef4cace1e10fdb1fa0d8650c3a4bf43
Ankur33657 <ankursingh943040@gmail.com>
2025-10-21 17:18:54 +0530

    feat:solve errro

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      router.push("/notes");
+    } else {
+      router.push("/login");
+    }
+  }, [router]);
+
+  return <h1>Loading...</h1>;
 }
--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,30 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_FILE_OR_PATH = [
-  "/login",
-  "/signup",
-  "/_next",
-  "/favicon.ico",
-  "/",
-];
+// const PUBLIC_FILE_OR_PATH = [
+//   "/login",
+//   "/signup",
+//   "/_next",
+//   "/favicon.ico",
+//   "/",
+// ];
 
 export function middleware(request: NextRequest) {
-  const authToken = request.cookies.get("token")?.value;
-  const { pathname } = request.nextUrl;
-
-  const isPublicPath = PUBLIC_FILE_OR_PATH.some(
-    (path) => pathname === path || pathname.startsWith(path),
-  );
-
-  if (!authToken && !isPublicPath) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (authToken && (pathname === "/login" || pathname === "/signup")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  const response = NextResponse.next();
-  response.headers.set("x-server-location", "edge-runtime");
-
-  return response;
+  //   const authToken = request.cookies.get("token")?.value;
+  //   const { pathname } = request.nextUrl;
+  //   const isPublicPath = PUBLIC_FILE_OR_PATH.some(
+  //     (path) => pathname === path || pathname.startsWith(path),
+  //   );
+  //   if (!authToken && !isPublicPath) {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   }
+  //   if (authToken && (pathname === "/login" || pathname === "/signup")) {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   }
+  //   const response = NextResponse.next();
+  //   response.headers.set("x-server-location", "edge-runtime");
+  //   return response;
+  // }
+  // export const config = {
+  //   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
